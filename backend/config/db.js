f52deeb6225764ce468c7eee235e516910a5dc3e
Manisha3435 <manishasinghrajput3435@gmail.com>
2026-05-19 23:30:25 +0530

    database connection add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mydb",
-  password: "Manisha@123",
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "mydb",
+        password: "Manisha@123",
+        port: 5432,
+      }
+);
 
 module.exports = pool;
